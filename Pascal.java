d77e647e69f7935c2060974d929dd63f48c1c17f
xushuangshuang <215881977@qq.com>
2018-09-22 11:52:33 +0800

    xiugai

--- Pascal.java
+++ Pascal.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class test {
+public class Pascal{
 	public static void main(String[] args)
 	{
 //		int[] number = null;
