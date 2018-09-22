f8eef31d49521fdb4c56a3041afa082c16dfa610
xushuangshuang <215881977@qq.com>
2018-09-22 11:28:44 +0800

    模板

---
+++ template.java
@@ -0,0 +1,26 @@
+package test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+public class template {
+	public static void main(String[] args)
+	{
+		Scanner x=new Scanner(System.in);
+		int[] number=null;
+		int m=x.nextInt();
+		number=new int[m];
+		 for(int i=0;i<m;i++) {
+	         number[i] = x.nextInt();
+	     }
+	   
+	     for(int j=0;j<m;j++) {
+	    	 System.out.println('1');
+	     }
+		
+	}
+	public static void test()
+	{
+		
+	}
+}
